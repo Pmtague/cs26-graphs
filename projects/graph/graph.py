e6fc42c3f9a933168719e0bfa2ba88a48c6dbdd9
Penny Tague <pmtague9@gmail.com>
2020-03-17 11:36:06 -0700

    dft, bft completed

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -5,48 +5,86 @@ from util import Stack, Queue  # These may come in handy
 
 class Graph:
 
-	"""Represent a graph as a dictionary of vertices mapping labels to edges."""
-	def __init__(self):
-		self.vertices = {}
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
 
-	def add_vertex(self, vertex_id):
-		"""
-		Add a vertex to the graph.
-		"""
-		# Adds another row to the vertices dictionary
-		self.vertices[vertex_id] = set()
+    def add_vertex(self, vertex_id):
+        """
+        Add a vertex to the graph.
+        """
+        # Adds another row to the vertices dictionary
+        self.vertices[vertex_id] = set()
 
-	def add_edge(self, v1, v2):
-		"""
-		Add a directed edge to the graph.
-		"""
-		if v1 in self.vertices and v2 in self.vertices:
-			self.vertices[v1].add(v2)
-		else:
-			raise ValueError("One or more vertices does not exist.")
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        # Adds the second vertex to the set of the first vertex
+        # For undirected edge, would also add first vertex to the set of the second vertex
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise ValueError("One or more vertices does not exist.")
 
-	def get_neighbors(self, vertex_id):
-		"""
-		Get all neighbors (edges) of a vertex.
-		"""
-		if vertex_id in self.vertices:
-			return self.vertices[vertex_id]
-		else:
-			raise ValueError("One or more vertices does not exist")
+    def get_neighbors(self, vertex_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
+        if vertex_id in self.vertices:
+            return self.vertices[vertex_id]
+        else:
+            raise ValueError("One or more vertices does not exist")
 
-	def bft(self, starting_vertex):
-		"""
-		Print each vertex in breadth-first order
-		beginning from starting_vertex.
-		"""
-		pass  # TODO
+    def bft(self, starting_vertex):
+        """
+        Print each vertex in breadth-first order
+        beginning from starting_vertex.
+        """
+        # Create a queue
+        q = Queue()
+        # Enqueue the starting vertex
+        q.enqueue(starting_vertex)
+        # Create a set to store the visited vertices
+        visited = set()
+        # While the queue is not empty
+        while q.size() > 0:
+            # Dequeue the first vertex
+            v = q.dequeue()
+            # Check if it's been visited
+            # If it hasn't been visited
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)
+                # Enqueue all it's neighbors
+                for neighbor in self.get_neighbors(v):
+                    q.enqueue(neighbor)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Create a stack
+        s = Stack()
+        # Push the starting vertex
+        s.push(starting_vertex)
+        # Create a set to store visited vertices
+        visited = set()
+        # While the stack is not empty...
+        while s.size() > 0:
+            # Pop the first vertex
+            v = s.pop()
+            # Check if it's been visited
+            # If not...
+            if v not in visited:
+                # Mark it visited
+                print(v)
+                visited.add(v)
+                # Push all it's neighbors onto the stack
+                for neighbor in self.get_neighbors(v):
+                    s.push(neighbor)
 
     def dft_recursive(self, starting_vertex):
         """
@@ -55,7 +93,7 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+        
 
     def bfs(self, starting_vertex, destination_vertex):
         """
