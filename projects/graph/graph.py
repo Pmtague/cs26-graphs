fd3c5b12a27113b4acb74ad1e9af6d5938976343
Penny Tague <pmtague9@gmail.com>
2020-03-17 09:43:03 -0700

    Initial commit

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -5,34 +5,41 @@ from util import Stack, Queue  # These may come in handy
 
 class Graph:
 
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        self.vertices = {}
+	"""Represent a graph as a dictionary of vertices mapping labels to edges."""
+	def __init__(self):
+		self.vertices = {}
 
-    def add_vertex(self, vertex_id):
-        """
-        Add a vertex to the graph.
-        """
-        pass  # TODO
+	def add_vertex(self, vertex_id):
+		"""
+		Add a vertex to the graph.
+		"""
+		# Adds another row to the vertices dictionary
+		self.vertices[vertex_id] = set()
 
-    def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        """
-        pass  # TODO
+	def add_edge(self, v1, v2):
+		"""
+		Add a directed edge to the graph.
+		"""
+		if v1 in self.vertices and v2 in self.vertices:
+			self.vertices[v1].add(v2)
+		else:
+			raise ValueError("One or more vertices does not exist.")
 
-    def get_neighbors(self, vertex_id):
-        """
-        Get all neighbors (edges) of a vertex.
-        """
-        pass  # TODO
+	def get_neighbors(self, vertex_id):
+		"""
+		Get all neighbors (edges) of a vertex.
+		"""
+		if vertex_id in self.vertices:
+			return self.vertices[vertex_id]
+		else:
+			raise ValueError("One or more vertices does not exist")
 
-    def bft(self, starting_vertex):
-        """
-        Print each vertex in breadth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
+	def bft(self, starting_vertex):
+		"""
+		Print each vertex in breadth-first order
+		beginning from starting_vertex.
+		"""
+		pass  # TODO
 
     def dft(self, starting_vertex):
         """
